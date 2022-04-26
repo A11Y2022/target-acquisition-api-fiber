4bbe96f7057634b5923672d9b541028d8eae2593
UpDJ <deionshallenberger@UCSF-MacBook-Pro.local>
2022-04-25 17:28:08 -0700

    fix for .env to work

--- testSession/test_model.go
+++
@@ -1,7 +0,0 @@
-package testSession
-
-type Config struct {
-	SubjectId  int    `json:"id" bson:"id"`
-	NumTrials  int    `json:"trials" bson:"trials"`
-	Difficulty string `json:"difficulty" bson:"difficulty"`
-}
